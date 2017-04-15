0425d744f3c9438d1a64c3126a9a91224b9c574c
Andy Ly <andyly@usc.edu>
2017-04-15 14:51:04 -0700

    Update Event.java

--- BaseClass/Event.java
+++ BaseClass/Event.java
@@ -3,61 +3,96 @@ package BaseClass;
 import java.util.Date;
 
 public class Event {
-	String title, eventID, locationgString, description;
+	int eventID;
+	String title, locationString, description;
 	int coordinateX, coordinateY;
 	User poster;
 	Date date;
-	public Event(String eventID, String title, String locationString, String description, Date date, int coordinateX, int coordinateY, User poster){
+
+	public Event(int eventID, String title, String locationString, String description, Date date, int coordinateX,
+			int coordinateY, User poster) {
 		this.title = title;
 		this.eventID = eventID;
-		this.locationgString = locationString;
+		this.locationString = locationString;
 		this.description = description;
 		this.coordinateX = coordinateX;
 		this.coordinateY = coordinateY;
 		this.date = date;
 		this.poster = poster;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	public String getEventID() {
+
+	public int getEventID() {
 		return eventID;
 	}
-	public void setEventID(String eventID) {
+
+	public void setEventID(int eventID) {
 		this.eventID = eventID;
 	}
-	public String getLocationgString() {
-		return locationgString;
+
+	public String getLocationString() {
+		return locationString;
 	}
-	public void setLocationgString(String locationgString) {
-		this.locationgString = locationgString;
+
+	public void setLocationString(String locationgString) {
+		this.locationString = locationgString;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public int getCoordinateX() {
 		return coordinateX;
 	}
+
 	public void setCoordinateX(int coordinateX) {
 		this.coordinateX = coordinateX;
 	}
+
 	public int getCoordinateY() {
 		return coordinateY;
 	}
+
 	public void setCoordinateY(int coordinateY) {
 		this.coordinateY = coordinateY;
 	}
+
+	public Date getDate() {
+		return this.date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
 	public User getPoster() {
 		return poster;
 	}
+
 	public void setPoster(User poster) {
 		this.poster = poster;
 	}
-	
+
+	public String eventTime() {
+		Date now = new Date();
+		if (date.compareTo(now) > 0) {
+			return "upcoming";
+		} else if (date.compareTo(now) < 0) {
+			return "past";
+		} else {
+			return "current";
+		}
+	}
 }
