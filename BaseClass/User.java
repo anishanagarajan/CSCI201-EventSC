f155f439319424ff88745f523ea66ef7a9460381
Andy Ly <andyly@usc.edu>
2017-04-15 14:52:00 -0700

    Update User.java

--- BaseClass/User.java
+++ BaseClass/User.java
@@ -3,62 +3,93 @@ package BaseClass;
 import java.util.ArrayList;
 
 public class User {
+	int userID;
 	String username, fname, lname;
-	float rating;
+	// float rating;
 	ArrayList<Event> previousEvents;
-	ArrayList<Event> myEvents;
+	ArrayList<Event> currentEvents;
 	ArrayList<Event> upcomingEvents;
-	public User(String username, String fname, String lname,float rating){
+
+	public User(int userID, String username, String fname, String lname) {
+		this.userID = userID;
 		this.username = username;
 		this.fname = fname;
 		this.lname = lname;
-		this.rating = rating;
-		myEvents = new ArrayList<Event>();
+		// this.rating = rating;
 		previousEvents = new ArrayList<Event>();
+		currentEvents = new ArrayList<Event>();
 		upcomingEvents = new ArrayList<Event>();
 	}
+
+	public int getUserID() {
+		return userID;
+	}
+
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
+	// public String getNickname() {
+	// return nickname;
+	// }
+	//
+	// public void setNickname(String nickname) {
+	// this.nickname = nickname;
+	// }
+
+	// public float getRating() {
+	// return rating;
+	// }
+	//
+	// public void setRating(float rating) {
+	// this.rating = rating;
+	// }
+
 	public String getFname() {
 		return fname;
 	}
-	public String getLname(){
-		return lname;
-	}
+
 	public void setFname(String fname) {
 		this.fname = fname;
 	}
-	public void setLname(String lname){
+
+	public String getLname() {
+		return lname;
+	}
+
+	public void setLname(String lname) {
 		this.lname = lname;
 	}
-	public float getRating() {
-		return rating;
-	}
-	public void setRating(float rating) {
-		this.rating = rating;
-	}
+
 	public ArrayList<Event> getPreviousEvents() {
 		return previousEvents;
 	}
+
 	public void setPreviousEvents(ArrayList<Event> previousEvents) {
 		this.previousEvents = previousEvents;
 	}
-	public ArrayList<Event> getMyEvents() {
-		return myEvents;
+
+	public ArrayList<Event> getCurrentEvents() {
+		return currentEvents;
 	}
-	public void setPreviousEvents(ArrayList<Event> myEvents) {
-		this.myEvents = myEvents;
+
+	public void setCurrentEvents(ArrayList<Event> currentEvents) {
+		this.currentEvents = currentEvents;
 	}
+
 	public ArrayList<Event> getUpcomingEvents() {
 		return upcomingEvents;
 	}
+
 	public void setUpcomingEvents(ArrayList<Event> upcomingEvents) {
 		this.upcomingEvents = upcomingEvents;
 	}
-	
-	
 }
