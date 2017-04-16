6afd556c945cfa4bd76922c23a2a69b3f8aea54e
Andy Ly <andyly@usc.edu>
2017-04-15 19:15:04 -0700

    Update PostManager.java

--- Managers/PostManager.java
+++ Managers/PostManager.java
@@ -1,9 +1,14 @@
 package Managers;
 
-public class PostManager { 
+import java.util.Date;
+
+import BaseClass.Event;
+import BaseClass.User;
+
+public class PostManager {
 	User currUser;
 	String title;
-	 
+	String time;
 	String location;
 	String description;
 	String category;
@@ -11,32 +16,44 @@ public class PostManager {
 	Date date;
 	float coordinateX;
 	float coordinateY;
-	public PostManager( User mUser, Date mDate, float mX, float mY){
+	DatabaseManager dm;
+
+	public PostManager(User mUser, Date mDate, float mX, float mY) {
 		currUser = mUser;
 		date = mDate;
 		coordinateX = mX;
 		coordinateY = mY;
+		dm = new DatabaseManager();
 	}
-	public void setTitle(String mTitle){
+
+	public void setTitle(String mTitle) {
 		title = mTitle;
 	}
-	 
-	public void setLocation(String mLocation){
+
+	public void setTime(String mTime) {
+		time = mTime;
+	}
+
+	public void setLocation(String mLocation) {
 		location = mLocation;
 	}
-	public void setDescription(String mDescription){
+
+	public void setDescription(String mDescription) {
 		description = mDescription;
 	}
-	public void setCategory(String mCategory){
+
+	public void setCategory(String mCategory) {
 		category = mCategory;
 	}
-	public void setImage(String mImage){
+
+	public void setImage(String mImage) {
 		image = mImage;
 	}
-	public void post(){
-		/*add code here*/
+
+	public void post() {
+		/* add code here */
 		Event newEvent = new Event(title, location, description, date, coordinateX, coordinateY, currUser);
 		dm.addEvent(newEvent);
 	}
-	
+
 }
